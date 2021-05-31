c25887503811ce5edbab32146b8fcf057155febc
Bosco <bosco11jico100@gmail.com>
2021-05-31 15:51:09 -0600

    Update config.php

--- marketplace/application/config/config.php
+++ marketplace/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'localhost/Proyecto2-progra4/marketplace';
 
 /*
 |--------------------------------------------------------------------------
